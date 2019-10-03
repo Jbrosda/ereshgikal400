b39b0681ac30d9c9127f2507bb7d8900109a9fec
Jbrosda <jbrosda@gmail.com>
2019-10-03 23:53:40 +0200

    added reverse sentence generation

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
-var extract_key_value_from_sentence = function (sentence) {
-    //append __END__ to signal end of sentence
-    sentence = sentence + " __END__"
+// converts a sentence from string to array, and throws out eccess "" values
+let clean_up_sentence = function (sentence) {
     // make !, , , ?, . , arrays of their own
     const quest_1 = /\?/gm;
     let sentence_repl_quest = sentence.replace(quest_1, " ? ")
@@ -13,32 +12,70 @@ var extract_key_value_from_sentence = function (sentence) {
     sentence = sentence_repl_exlam;
 
     var words = sentence.split(" ")
-    //remove 2double and more key"" from array
-    let cleanup_words = function (input_array) {
-        let clean = []
-        for (i = 0; i < input_array.length; i++) {
-            if (input_array[i] === "" && input_array[i + 1] === "") {
-                i++
-            } else {
-                clean.push(input_array[i])
-            }
-        }
-        return clean
-    }
-    words = cleanup_words(words)
-    
-    var result = {}
-    for (var i = 0; i < words.length - 3; i++) {
-        var three_words = words[i] + " " + words[i + 1] + " " + words[i + 2]
-        var next_word = words[i + 3];
-        result[three_words] = next_word;
-    }
-    return result
-}
-// here is how the words are stored
-var storage_dictionary = {}
 
-var store_key_values = function (key_values) {
+    return words
+}
+//remove 2double and more key"" from array
+let cleanup_words = function (input_array) {
+    let clean = []
+    for (i = 0; i < input_array.length; i++) {
+        if (input_array[i] === "" && input_array[i + 1] === "") {
+            i++
+        } else {
+            clean.push(input_array[i])
+        }
+    }
+    return clean
+}
+// outputs an Object with two sub_objects
+var extract_key_value_from_sentence = function (sentence) {
+    //append __END__ to signal end of sentence
+    let normal_sentence = sentence
+    let words = clean_up_sentence(normal_sentence)
+    words = cleanup_words(words);
+
+    let words_fwd = words.slice();
+    //console.log(words_fwd)
+   words_fwd.push("__END__");
+   const end_words_fwd = words_fwd
+   //console.log(end_words_fwd);
+
+    const reverse_words = words.slice();
+    reverse_words.reverse();
+    reverse_words.push("__END__");
+    //console.log(reverse_words)
+
+    //makes an object from an array
+    let generate_result = function (words) {
+        let resulting_Object = {}
+        for (var i = 0; i < words.length - 3; i++) {
+            var three_words = words[i] + " " + words[i + 1] + " " + words[i + 2]
+            var next_word = words[i + 3];
+            resulting_Object[three_words] = next_word;
+        }
+        return resulting_Object
+    }
+    let result_Object = generate_result(end_words_fwd)
+    // makes an object containing two objects, normal for sentence after keyword, reverse  for sentence before keyword
+    let reverse_result_Object = generate_result(reverse_words)
+    let mixed_object = {
+        normal: result_Object,
+        reverse: reverse_result_Object
+    }
+    return mixed_object
+}
+
+//storage dictionary now contains two objects: nprmal
+let storage_dictionary = {
+    normal: {},
+    reverse: {}
+}
+
+// // here is how the words are stored
+var storage_dictionary_fwd = {}
+let storage_dictionary_rev = {}
+
+var store_key_values = function (storage_dictionary, key_values) {
     var keys = Object.keys(key_values)
     for (var i = 0; i < keys.length; i++) {
         var three_words = keys[i]
@@ -53,12 +90,13 @@ var store_key_values = function (key_values) {
         }
     }
 }
-var reconstruct_sentence = function (three_words) {
+
+var reconstruct_sentence_fwd = function (three_words) {
     // create a collector with three words as initial value
     var result = three_words;
     while (true) {
         // get possible next words and occurance counts
-        var possible_next_words_with_occurance = storage_dictionary[three_words];
+        var possible_next_words_with_occurance = storage_dictionary.normal[three_words];
         // most frequent
         var most_frequent_next_word;
         var max_occurance = 0;
@@ -66,12 +104,10 @@ var reconstruct_sentence = function (three_words) {
         var possible_next_words = Object.keys(possible_next_words_with_occurance);
         var random_index = Math.floor(Math.random() * possible_next_words.length);
         var next_word = possible_next_words[random_index];
-
-
-
-        if (next_word === "__END__") {
+        if (next_word === "__END__" || next_word === "__START__") {
             return result;
         }
+        
         // the next lines are for most used  next words
         // for (var possible_next_word of possible_next_words) {
         //     var occurance = possible_next_words_with_occurance[possible_next_word]
@@ -95,18 +131,63 @@ var reconstruct_sentence = function (three_words) {
         // change key
         three_words = three_words_split.join(" ");
     }
+}
+var reconstruct_sentence_rev = function (three_words) {
+    // create a collector with three words as initial value
+    var result = three_words;
+    
+    while (true) {
+        // get possible next words and occurance counts
+        var possible_next_words_with_occurance = storage_dictionary.reverse[three_words];
+        // most frequent
+        var most_frequent_next_word;
+        var max_occurance = 0;
+        //console.log(three_words, possible_next_words_with_occurance)
+        var possible_next_words = Object.keys(possible_next_words_with_occurance);
+        var random_index = Math.floor(Math.random() * possible_next_words.length);
+        var next_word = possible_next_words[random_index];
+        if (next_word === "__START__" || next_word === "__END__"){
+            return result
+        }
+        // the next lines are for most used  next words
+        // for (var possible_next_word of possible_next_words) {
+        //     var occurance = possible_next_words_with_occurance[possible_next_word]
+        //     if (occurance > max_occurance) {
+        //         max_occurance = occurance;
+        //         most_frequent_next_word = possible_next_word;
+        //     }
+        // }
+        // var next_word = most_frequent_next_word
 
-};
+        // if we reach an ending of sentence
+
+        // add to result
+        result = next_word + " " + result
+        // change the original three words
+        var three_words_split = three_words.split(" ")
+        // remove the first of three
+        three_words_split.shift();
+        //add the next word to the three words
+        three_words_split.push(next_word)
+        // change key
+        three_words = three_words_split.join(" ");
+    }
+}
 var store_text = function (text) {
     var sentences = text.split(".");
     for (var sentence of sentences) {
         var sentence_key_value = extract_key_value_from_sentence(sentence.trim())
-        store_key_values(sentence_key_value)
+        store_key_values(storage_dictionary.normal, sentence_key_value.normal)
+        store_key_values(storage_dictionary.reverse, sentence_key_value.reverse)
     }
 }
-//console.log(storage_dictionary);
+
+
 module.exports = {
     store_text: store_text,
-    reconstruct_sentence: reconstruct_sentence,
+    reconstruct_sentence_fwd: reconstruct_sentence_fwd,
+    reconstruct_sentence_rev: reconstruct_sentence_rev,
     storage_dictionary: storage_dictionary,
+    clean_up_sentence, cleanup_words
+
 }
--- index.js
+++ index.js
@@ -6,7 +6,16 @@ var read_book = function (src) {
 
     bot.store_text(book_1);
 }
+// helpers
+let minimum_length = function (length1, length2) {
+    if (length1 > length2) {
+        return length2
+    } else {
+        return length1
+    }
+}
 //read from this dictionary
+
 
 var process_books = function (error, booknames) {
     for (let i = 0; i < booknames.length; i++) {
@@ -24,64 +33,46 @@ var current_book_name = "./books/" + new Date() + '.txt';
 // how it fixes the text that was inserted
 
 var sentence_selection = function (input_text) {
-    const quest_1 = /\?/gm;
-    let sentence_repl_quest = input_text.replace(quest_1, " ? ")
-    const comma_1 = /\,/gm;
-    let sentence_repl_comma = sentence_repl_quest.replace(comma_1, " , ")
-    const stop_1 = /\./gm;
-    let sentence_repl_stop = sentence_repl_comma.replace(stop_1, " . ")
-    const exclamation_1 = /\!/gm;
-    let sentence_repl_exlam = sentence_repl_stop.replace(exclamation_1, " ! ")
-    sentence = sentence_repl_exlam;
-
-    var input_words = sentence_repl_exlam.split(" ")
-    // remove extra ""empty arrays
-
-    let cleanup_words = function (input_array) {
-        let clean = []
-        for (i = 0; i < input_array.length; i++) {
-            if (input_array[i] === "" && input_array[i + 1] === "") {
-                i++
-            } else {
-                clean.push(input_array[i])
-            }
-        }
-        return clean
-    }
-    input_words = cleanup_words(input_words)
+    //cleanup of the input string
+    let input_words = bot.clean_up_sentence(input_text);
+    input_words = bot.cleanup_words(input_words)
 
     // this selects a word out of the input 
     // expand this section!!!!
     let selected_word = function (word) {
         var input_index = Math.floor(Math.random() * input_words.length)
         let the_word = "";
-        if (word[input_index] === "" || word[input_index] === "." || word[input_index] === "," || word[input_index] === "!" ||word[input_index] === "?"){
-             the_word = input_words[input_index - 1] 
-        }else{
+        if (word[input_index] === "" || word[input_index] === "." || word[input_index] === "," || word[input_index] === "!" || word[input_index] === "?") {
+            the_word = input_words[input_index - 1]
+        } else {
             the_word = input_words[input_index]
         }
         return the_word
-} 
+    }
 
     selected_word = selected_word(input_words)
-    //old function
-    // var input_index = Math.floor(Math.random() * input_words.length)
-    // var selected_word = input_words[input_index]
-
-
-
-
     console.log("selected " + selected_word)
 
     // this starts the search of the answer
-    var possible_three_words = Object.keys(bot.storage_dictionary).filter(function (three_words) {
-        return three_words.indexOf(selected_word) > -1
+    let possible_three_words_forward = Object.keys(bot.storage_dictionary.normal).filter(function (three_words) {
+        return three_words.indexOf(selected_word) > -1;
     });
+    let possible_three_words_reverse = Object.keys(bot.storage_dictionary.reverse).filter(function (three_words) {
+        return three_words.indexOf(selected_word) > -1;
+    });
+    //console.log(possible_three_words_reverse)
+    if (possible_three_words_forward.length > 0) {
+        // reconstruct has to access inner object
+        var random_index_fwd = Math.floor(Math.random() *possible_three_words_forward.length)
+        let random_index_rev = Math.floor(Math.random() *possible_three_words_reverse.length)
+        let selected_three_words_fwd = possible_three_words_forward[random_index_fwd]
+        let selected_three_words_rev = possible_three_words_reverse[random_index_rev]
 
-    if (possible_three_words.length > 0) {
-        var random_index = Math.floor(Math.random() * possible_three_words.length);
-        var selected_three_words = possible_three_words[random_index]
-        var new_sentence = bot.reconstruct_sentence(selected_three_words);
+        let new_sentence_fwd = bot.reconstruct_sentence_fwd(selected_three_words_fwd);
+        let new_sentence_rev = bot.reconstruct_sentence_rev(selected_three_words_rev);
+        
+        let new_sentence = new_sentence_rev +" "+ new_sentence_fwd
+        return new_sentence
     } else {
         var new_sentence = "Tell me more, talking about " + selected_word + " sounds interresting"
     }
@@ -95,41 +86,43 @@ var sentence_selection = function (input_text) {
 //console.log(bot.storage_dictionary)
 
 // BOT CONNEXION
+let the_slack_botter= function () {
+    var SlackBot = require('slackbots');
 
-var SlackBot = require('slackbots');
-
-// create a bot
-var config = require("./config");
-var slack_bot = new SlackBot(config);
-
-slack_bot.on('start', function () {
-    var user = slack_bot.users.find(function (user) {
-        if (user.real_name === slack_bot.name) {
-            return true
-        }
-    })
-    slack_bot.id = user.id
-
-    // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
-});
-slack_bot.on('message', function (data) {
-    if (data.type !== "message" || !data.text || data.user === slack_bot.id) {
-        return
-    }
-    // all ingoing events https://api.slack.com/rtm
-    console.log(data.text);
-    var mentioned = `<@${slack_bot.id}>`;
-    var random = Math.random() * 100;
-    if (data.text && (data.text.indexOf(mentioned) > -1) || random > 80) {
-        var channel = slack_bot.channels.find(function (channel) {
-            return channel.id === data.channel
+    // create a bot
+    var config = require("./config");
+    var slack_bot = new SlackBot(config);
+    
+    slack_bot.on('start', function () {
+        var user = slack_bot.users.find(function (user) {
+            if (user.real_name === slack_bot.name) {
+                return true
+            }
         })
-        var reply = sentence_selection(data.text)
-        reply = reply.replace(/<@self>/g, `<@${data.user}>`)
-        slack_bot.postMessageToChannel(channel.name, sentence_selection(data.text));
-    }
-    data.text = data.text.replace(new RegExp(mentioned), "<@self>")
-    bot.store_text(data.text)
-
-    fs.appendFileSync(current_book_name, data.text + ' . ')
-});
+        slack_bot.id = user.id
+    
+        // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
+    });
+    slack_bot.on('message', function (data) {
+        if (data.type !== "message" || !data.text || data.user === slack_bot.id) {
+            return
+        }
+        // all ingoing events https://api.slack.com/rtm
+        console.log(data.text);
+        var mentioned = `<@${slack_bot.id}>`;
+        var random = Math.random() * 100;
+        if (data.text && (data.text.indexOf(mentioned) > -1) || random > 80) {
+            var channel = slack_bot.channels.find(function (channel) {
+                return channel.id === data.channel
+            })
+            var reply = sentence_selection(data.text)
+            reply = reply.replace(/<@self>/g, `<@${data.user}>`)
+            slack_bot.postMessageToChannel(channel.name, sentence_selection(data.text));
+        }
+        data.text = data.text.replace(new RegExp(mentioned), "<@self>")
+        bot.store_text(data.text)
+    
+        fs.appendFileSync(current_book_name, data.text + ' . ')
+    });
+}
+the_slack_botter()
